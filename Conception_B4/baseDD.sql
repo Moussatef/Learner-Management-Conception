7c5d26c13879c2bc594833f0387a9731da57194d
Moussatef <otman.moussetaf@gmail.com>
2021-02-25 23:24:48 +0100

    modification on SQL DATA BASE

--- Conception_B4/baseDD.sql
+++ Conception_B4/baseDD.sql
@@ -28,22 +28,13 @@ drop table if exists PRESSON_ETUD;
 create table ADMIN
 (
    ID_ADMIN             int not null,
-   NOM                  char(15),
-   EMAIL                char(25),
-   PASSWORD             char(50),
+   NOM                  varchar(15),
+   EMAIL                varchar(25),
+   PASSWORD             varchar(50),
    primary key (ID_ADMIN)
 );
 
-/*==============================================================*/
-/* Table : CLASSE_ETD                                           */
-/*==============================================================*/
-create table CLASSE_ETD
-(
-   ID_CLASSE_ETD        int not null,
-   ID_FORMATEUR         int not null,
-   NOM                  char(10),
-   primary key (ID_CLASSE_ETD)
-);
+
 
 /*==============================================================*/
 /* Table : ETUDIANT                                             */
@@ -65,12 +56,23 @@ create table FORMATEUR
 );
 
 /*==============================================================*/
+/* Table : CLASSE_ETD                                           */
+/*==============================================================*/
+create table CLASSE_ETD
+(
+   ID_CLASSE_ETD        int not null,
+   ID_FORMATEUR         int foreign key references FORMATEUR(ID_FORMATEUR) not null,
+   NOM                  varchar(10),
+   primary key (ID_CLASSE_ETD,ID_FORMATEUR)
+);
+
+/*==============================================================*/
 /* Table : MODULE                                               */
 /*==============================================================*/
 create table MODULE
 (
    ID_MODULE            int not null,
-   NOM_MODULE           char(50),
+   NOM_MODULE           varchar(50),
    primary key (ID_MODULE)
 );
 
@@ -93,11 +95,11 @@ create table NOTES
 create table PERSONE
 (
    ID_PERSON            int not null,
-   NOM                  char(15),
-   PRENOM               char(20),
+   NOM                  varchar(15),
+   PRENOM               varchar(20),
    AGE                  int,
-   EMAIL                char(25),
-   PASSWORD             char(50),
+   EMAIL                varchar(25),
+   PASSWORD             varchar(50),
    primary key (ID_PERSON)
 );
 
@@ -120,6 +122,20 @@ create table PRESSON_ETUD
    ID_PERSON            int not null,
    primary key (ID_ETUDIANT, ID_PERSON)
 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 alter table CLASSE_ETD add constraint FK_CLASSE_FORMATEUR2 foreign key (ID_FORMATEUR)
       references FORMATEUR (ID_FORMATEUR) on delete restrict on update restrict;
